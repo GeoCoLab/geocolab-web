857a935c82a6674225cab3bbbfb648657b87548a
Ginger Butcher <alycejenni@gmail.com>
2022-06-08 11:30:46 +0100

    fix: add new user fields

--- pages/user/register.page.vue
+++ pages/user/register.page.vue
@@ -7,9 +7,15 @@
     <FormKit type="password" name="password_confirm" label="Confirm Password" validation="required|confirm"
              validation-label="Password" />
 
-    <FormKit type="text" name="name" label="Name" validation="required" />
+    <FormKit type="text" name="family_name" label="Family name" validation="required" />
+    <FormKit type="text" name="given_name" label="Given name(s)" validation="required" />
+    <FormKit type="select" name="given_name_first"
+             :options="[{label:'Given name first', value: 1},{label:'Family name first', value: 0}]"
+             label="Name display"
+             :help="`Display your ${formData.given_name_first ? 'given name' : 'family name'} first, e.g. ${displayName}`" />
 
-    <CountrySelect name="country" label="Country" v-model="formData.country"/>
+    <CountrySelect name="country" label="Country" v-model="formData.country" help="Your current country of residence"/>
+    <FormKit type="text" name="pronouns" label="Pronouns" help="e.g. he/him, she/her, they/them"/>
     <div class="form-row-custom">
       <div class="form-row-inputs">
         <FormKit type="submit" @click="registerManager">Register as manager</FormKit>
@@ -23,11 +29,11 @@
 </template>
 
 <script setup>
-import { ref } from 'vue';
+import { computed, ref } from 'vue';
 import { navigate } from 'vite-plugin-ssr/client/router';
 import { useEnumStore } from '../../store/enums';
 import { useAuthStore } from '../../store/auth';
-import CountrySelect from '../../components/CountrySelect.vue'
+import CountrySelect from '../../components/CountrySelect.vue';
 
 const enums = useEnumStore();
 const auth = useAuthStore();
@@ -41,21 +47,21 @@ function doNothing() {
 
 async function submitHandler() {
   return auth.register(formData)
-      .then(r => {
-        if (r.status >= 400) {
-          errors.value = r.data.errors;
-          return false;
-        } else {
-          return true;
-        }
-      });
+             .then(r => {
+               if (r.status >= 400) {
+                 errors.value = r.data.errors;
+                 return false;
+               } else {
+                 return true;
+               }
+             });
 }
 
 async function registerResearcher() {
   formData.value['role'] = 'researcher';
   submitHandler().then(success => {
     if (success) {
-      navigate('/researcher/register')
+      navigate('/researcher/register');
     }
   });
 }
@@ -64,10 +70,18 @@ async function registerManager() {
   formData.value['role'] = 'manager';
   submitHandler().then(success => {
     if (success) {
-      navigate('/manager/register')
+      navigate('/manager/register');
     }
   });
 }
+
+const displayName = computed(() => {
+  if (formData.value['given_name_first']) {
+    return [formData.value['given_name'] || 'Jane', formData.value['family_name'] || 'Doe'].join(' ');
+  } else {
+    return [formData.value['family_name'] || 'Yamada', formData.value['given_name'] || 'Hanako'].join(' ');
+  }
+});
 
 </script>
 
