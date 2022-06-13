7ae45c3b36772bede81418b7c8612877ddb4e772
Ginger Butcher <alycejenni@gmail.com>
2022-06-13 14:38:59 +0100

    fix: this is getting silly

--- utils/api.ts
+++ utils/api.ts
@@ -26,7 +26,7 @@ api.interceptors.response.use(
     },
     async function (error) {
         let originalRequest = error.config;
-        console.error(error.response);
+        console.error(error);
         if (error.response.data.errors.includes('access token has expired')) {
             try {
                 let cookieString = originalRequest.headers['Cookie']
