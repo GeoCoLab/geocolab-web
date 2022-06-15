9b7f3c2d6625207cb1f129f1e93a8fe9982cfcc3
Ginger Butcher <alycejenni@gmail.com>
2022-06-15 12:02:10 +0100

    feat: add new blog post button for admins

--- pages/blog/index.page.vue
+++ pages/blog/index.page.vue
@@ -1,5 +1,6 @@
 <template>
   <h2 class="page-title">Blog</h2>
+  <div v-if="pageContext.user.role === 'admin'" class="flex justify-end"><a href="/blog/new" class="button">New Post</a></div>
   <div v-for="p in summaries" class="blog-summary">
     <h3><a :href="`/blog/post/${p.slug}`">{{ p.title }}</a></h3>
     <small>{{ p.posted }}</small>
@@ -8,7 +9,10 @@
 </template>
 
 <script setup>
+import {usePageContext} from '../../utils/usePageContext';
+
 defineProps(['summaries'])
+const pageContext = usePageContext();
 </script>
 
 <style scoped lang="scss">
