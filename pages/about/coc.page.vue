51a8f03256231679a2682174d0f85b87f1f197e3
Ginger Butcher <alycejenni@gmail.com>
2022-07-07 12:07:42 +0100

    feat: add code of conduct

--- pages/about/coc.page.vue
+++ pages/about/coc.page.vue
@@ -1,6 +1,104 @@
 <template>
   <h2 class="page-title">Code of Conduct</h2>
-  <p>#TODO</p>
+  <article class="prose lg:prose-lg">
+    <p>GeoCoLab aims to support ethical, open science. This document is not intended to be an exhaustive Code of
+       Conduct,
+       but rather to encourage fair and equitable collaborations between research users and analytical facilities
+       matched
+       through GeoCoLab. We refer users to the UK Research Integrity Office for an exemplar <a
+          href="https://ukrio.org/publications/code-of-practice-for-research/1-0-introduction/" target="_blank">Code of
+                                                                                                                Practice</a>.
+    </p>
+    <p>We expect research users and analytical facilities to act in good faith and abide by the respective Memorandum of
+       Understanding and Terms in Conditions. We promote open dialogue, setting clear expectations and mutual agreement
+       of
+       any collaboration. Once matched, users and laboratories should discuss and agree a programme of work. We
+       recommend
+       that this includes the following:</p>
+    <ul class="list-disc">
+      <li>Number of samples or instrument time</li>
+      <li>Preparation of samples</li>
+      <li>Data delivery mode e.g. raw data, or interpreted data</li>
+      <li>Schedule of analysis and data delivery, including review points</li>
+      <li>Authorship of resulting publications</li>
+      <li>Acknowledgement in resulting publications</li>
+      <li>IP, data ownership and data sharing</li>
+    </ul>
+    <p>That agreement takes place between the laboratory and the user. If an agreement cannot be reached, the match can
+       be
+       dissolved. GeoCoLab cannot remediate any disagreements nor be held responsible for any contraventions of that
+       agreement.</p>
+    <h3>Inclusivity and Diversity</h3>
+    <p>Enjoyable, high-quality research can only be conducted if people feel safe, secure, and supported. GeoCoLab
+       expects
+       a harassment-free experience for everyone, regardless of gender identity and expression, sexual orientation,
+       disability, physical appearance, body size, race, age, and/or religion. We do not tolerate harassment by and/or
+       of
+       users in any form, and we ask all members of the community to conform to the following Code of Conduct:</p>
+    <ul class="list-disc">
+      <li>All communication, be it online or in person, should be appropriate for a professional audience, and be
+          considerate of people from different cultural backgrounds. Sexual language and imagery is not appropriate at
+          any
+          time.
+      </li>
+      <li>Behave professionally. Remember that harassment and sexist, racist, or exclusionary jokes are not appropriate.
+      </li>
+      <li>Harassment includes offensive verbal comments related to gender, sexual orientation, disability, physical
+          appearance, body size, race, religion, sexual images in public spaces, deliberate intimidation, stalking,
+          following, harassing photography or recording, sustained disruption of discussions, inappropriate physical
+          contact, and unwelcome sexual attention.
+      </li>
+      <li>Participants asked to stop any harassing behaviour are expected to comply immediately.</li>
+      <li>Collaborate with a constructive, positive approach.</li>
+    </ul>
+
+    <h3>GeoCoLab acknowledgement protocol</h3>
+    <p>You must acknowledge GeoCoLab’s support in all your research publications and other outputs. For example, please
+       use the following text in acknowledgements: ”This work was supported by GeoCoLab, funded by NERC
+       NE/W007622/1”.</p>
+
+    <h3>Further guidance for Users</h3>
+    <p>It is important to consider the nature of your research collaboration with your matched laboratory. Consider
+       questions like: Do you need analysis only? Or specific expertise and therefore intellectual property? Often
+       called
+       IP for short. If they are providing analysis only you will know exactly what analysis you want and why. This is
+       more like a ‘service contract’.</p>
+    <p>In these cases you will need to, at the least, acknowledge the laboratory and GeoCoLab funding, see GeoCoLab
+       acknowledgement protocol, in the acknowledgement section of any publication. Anything that is not a ‘service
+       contract’ will involve IP of the laboratory or that of the individuals that work there. Many laboratories will
+       have
+       an IP and/or data agreement and you should not be surprised if you are asked to sign one. Irrespective of whether
+       this is the case, or not, you should collectively (yourself and the laboratory) consider IP, data ownership and
+       use
+       at the start of the project to protect your IP and that of the collaborating laboratory and individuals. This
+       could
+       include rights of data use, paper authorship, acknowledgements etc. It is useful to have a plan to revisit any
+       agreements as the project evolves to ensure all parties are happy that any original agreement is still fair and
+       covers any changes to project input and circumstances.</p>
+    <p>Laboratories, or their host institutions, may also expect you to sign-up to ethical agreements and codes of
+       practice, especially if you are visiting a laboratory to conduct the analyses yourself. This is all normal, and
+       best practice, it is designed to protect you, your collaborators and others. These agreements will be in addition
+       to any ethical statements you agreed to as part of your GeoCoLab match.</p>
+    <p>When you are thinking about what analyses you need and level of input, it is might be useful to consider the
+       following questions:</p>
+    <ul class="list-disc">
+      <li>What support and input do you need?</li>
+      <li>Will you complete the analyses yourself?</li>
+      <li>Will you prepare the samples?</li>
+      <li>Would you like input into interpretation of the results?</li>
+      <li>Would you like input into writing methods e.g. the use of standards, instrument set-up…?</li>
+    </ul>
+    <p>Answers to such questions will help you to determine the extent of any collaboration agreement and can help frame
+       a
+       discussion.</p>
+    <p>We promote open science and recommend pre-printing your work. You should agree with the analytical facility
+       whether
+       they are included as authors, or in the acknowledgements. All authors, including those from the analytical
+       facility, should agree to authorship prior to the submission of a manuscript for publication. Upon completion of
+       the project and where data confidentiality allows, consider placing all data in an appropriate repository with a
+       DOI; in the case of publication, these data should be published alongside the manuscript on a repository.</p>
+  </article>
+
 </template>
 
 <style lang="scss">
