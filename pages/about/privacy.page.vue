62ef1b59f8e5b5783fac583dc7d9044e9dd15ee2
Ginger Butcher <alycejenni@gmail.com>
2022-07-22 14:01:36 +0100

    feat: add placeholder terms and privacy policy

---
+++ pages/about/privacy.page.vue
@@ -0,0 +1,9 @@
+<template>
+  <h2 class="page-title">Privacy Policy</h2>
+  <article class="prose lg:prose-lg">
+    <p>Eventually, a privacy policy will be here.</p>
+  </article>
+</template>
+
+<style lang="scss">
+</style>
