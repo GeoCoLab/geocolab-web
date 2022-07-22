496f6a6ac13cfddcd09496a58f02b7d61a5bde29
Ginger Butcher <alycejenni@gmail.com>
2022-07-22 13:58:48 +0100

    feat: add util method for new/edit routes

--- utils/misc.ts
+++ utils/misc.ts
@@ -1,5 +1,8 @@
 import moment from 'moment';
-import { StringDict } from '../types';
+import { PageContextWrapper, StringDict } from '../types';
+import partRegex from 'part-regex';
+import { api } from './api';
+import { PageContextBuiltIn } from 'vite-plugin-ssr';
 
 export function hasExpired(date: string) {
     return moment(date).isAfter(moment.now());
@@ -29,4 +32,32 @@ export function getCookies() {
         cookies[cookiename] = cookievalue.join('=');
     });
     return cookies;
+}
+
+export function newOrEditRoute(rootPath: string, permittedRoles: Array<string>) {
+    return async (pageContext: PageContextWrapper) => {
+        const {url} = pageContext;
+        const editItem = partRegex`${ rootPath }/edit/${ /[a-z0-9-]+/ }`.test(url);
+        const newItem = url === `${ rootPath }/new`;
+        if ((!editItem) && (!newItem)) {
+            return false;
+        }
+
+        if (!pageContext.pageContext.user) {
+            // if no user already in page context, recheck the API
+            pageContext.pageContext.user = await api.get('/user').then(r => r.data);
+        }
+        if (pageContext.pageContext.user.role && !permittedRoles.includes(pageContext.pageContext.user.role)) {
+            return false;
+        }
+
+        let urlParts = url.split('/');
+        const itemId = urlParts[urlParts.length - 1];
+
+        return {
+            routeParams: {
+                itemId
+            }
+        };
+    };
 }
